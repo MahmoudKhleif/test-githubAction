b7d863953fba2144cc785645afa37fe35a1393b7
Mahmoud Khleif <Mahmoud89ah@gmail.com>
2021-12-20 20:28:56 +0100

    Add project files.

---
+++ test/räcknare.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class räcknare
+    {
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double SquareRoot(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
---
+++ testTests/räcknareTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Tests
+{
+    [TestClass()]
+    public class räcknareTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.Add(5, 5);
+            var expected = 10;
+
+            Assert.AreEqual(expected, actul);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.Multiply(5, 5);
+            var expected = 25;
+            Assert.AreEqual(expected, actul);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.Multiply(5, 5);
+            var expected = 25;
+            Assert.AreEqual(expected, actul);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.Divide(5, 5);
+            var expected = 1;
+            Assert.AreEqual(expected, actul);
+        }
+
+        [TestMethod()]
+        public void PowerTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.Power(5, 2);
+            var expected = 25;
+            Assert.AreEqual(expected, actul);
+        }
+
+        [TestMethod()]
+        public void SquareRootTest()
+        {
+            var cul = new räcknare();
+            var actul = cul.SquareRoot(5);
+            var expected = 2.23606797749979;
+            Assert.AreEqual(expected, actul);
+        }
+    }
+}
